68a060d8afd39c72333e29d62be73ae074645a4a
Oracio Carvalho Schopping <oracio.j@hotmail.com>
2025-11-15 18:40:02 -0300

    DELETE.SQL.V1

---
+++ sql/dml/delete.sql
@@ -0,0 +1,47 @@
+-- Excluir pessoa física
+DELETE FROM Pessoa_Fisica
+WHERE id_pf = ;
+
+-- Excluir pessoa jurídica
+DELETE FROM Pessoa_Juridica
+WHERE id_pj = ;
+
+-- Excluir aluno
+DELETE FROM Aluno
+WHERE id_aluno = ;
+
+-- Excluir professor
+DELETE FROM Professor
+WHERE id_professor = ;
+
+-- Excluir funcionário
+DELETE FROM Funcionario
+WHERE id_funcionario = ;
+
+-- Excluir fornecedor
+DELETE FROM Fornecedor
+WHERE id_fornecedor = ;
+
+-- Excluir patrimônio
+DELETE FROM Patrimonio
+WHERE id_patrimonio = ;
+
+-- Excluir compra
+DELETE FROM Compra
+WHERE id_compra = ;
+
+-- Excluir item de compra
+DELETE FROM Item_Compra
+WHERE id_item = ;
+
+-- Excluir livro
+DELETE FROM Livro
+WHERE id_livro = ;
+
+-- Excluir empréstimo
+DELETE FROM Emprestimo
+WHERE id_emprestimo = ;
+
+-- Excluir pagamento
+DELETE FROM Pagamento
+WHERE id_pagamento = ;
