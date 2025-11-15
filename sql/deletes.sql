9953efe252f755fc3e01c4def9c54b218fc18838
Oracio Carvalho Schopping <oracio.j@hotmail.com>
2025-11-15 19:35:23 -0300

    SQL.V13

--- sql/deletes.sql
+++ sql/deletes.sql
@@ -1,47 +1,29 @@
--- Deletar pessoa física
-DELETE FROM Pessoa_Fisica
-WHERE id = ?;
+-- Excluir registros de Pessoa
+DELETE FROM Pessoa WHERE id = ...;
 
--- Deletar pessoa jurídica
-DELETE FROM Pessoa_Juridica
-WHERE id = ?;
+-- Excluir registros de Pessoa Física
+DELETE FROM PessoaFisica WHERE id = ...;
 
--- Deletar aluno
-DELETE FROM Aluno
-WHERE id = ?;
+-- Excluir registros de Pessoa Jurídica
+DELETE FROM PessoaJuridica WHERE id = ...;
 
--- Deletar professor
-DELETE FROM Professor
-WHERE id = ?;
+-- Excluir registros de Fornecedor
+DELETE FROM Fornecedor WHERE id = ...;
 
--- Deletar funcionário
-DELETE FROM Funcionario
-WHERE id = ?;
+-- Excluir registros de Professor
+DELETE FROM Professor WHERE id = ...;
 
--- Deletar fornecedor
-DELETE FROM Fornecedor
-WHERE id = ?;
+-- Excluir registros de Aluno
+DELETE FROM Aluno WHERE id = ...;
 
--- Deletar patrimônio
-DELETE FROM Patrimonio
-WHERE id = ?;
+-- Excluir registros de Funcionário
+DELETE FROM Funcionario WHERE id = ...;
 
--- Deletar compra
-DELETE FROM Compra
-WHERE id = ?;
+-- Excluir registros de Biblioteca
+DELETE FROM Biblioteca WHERE id = ...;
 
--- Deletar item de compra
-DELETE FROM Item_Compra
-WHERE id = ?;
+-- Excluir registros de Livro
+DELETE FROM Livro WHERE id = ...;
 
--- Deletar livro
-DELETE FROM Livro
-WHERE id = ?;
-
--- Deletar empréstimo
-DELETE FROM Emprestimo
-WHERE id = ?;
-
--- Deletar pagamento
-DELETE FROM Pagamento
-WHERE id = ?;
+-- Excluir registros de Empréstimo
+DELETE FROM Emprestimo WHERE id = ...;
